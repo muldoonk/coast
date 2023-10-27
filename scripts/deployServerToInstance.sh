510349c2de1305c140a4b1ccc7ecc8226cb988f1
Katie Muldoon <krmuldoo@gmail.com>
2023-10-27 07:42:59 -0400

    feat: refactor server into it's own directory

--- scripts/deployServerToInstance.sh
+++ scripts/deployServerToInstance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # not to be included in copy operation
-rm -rf node_modules
+rm -rf coast-server/node_modules
 
 ## delete old files
 ssh -i $1 ec2-user@$2 "rm -rf /home/ec2-user/coast"
